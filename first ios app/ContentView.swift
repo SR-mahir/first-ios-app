380a98a245d6e16d414ad187d2644812890da08e
mahir <srmahir98@gmail.com>
2025-12-16 00:20:49 +0600

    Update text

--- first ios app/ContentView.swift
+++ first ios app/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("YAY!! Finally got git integrated!")
         }
         .padding()
     }
